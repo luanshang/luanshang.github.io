0c9f735250285dee033a0373eabf514878905379
luanshang <3092078599@qq.com>
2023-10-17 20:50:37 +0800

    Site updated: 2023-10-17 20:50:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/17/hello-world/","2023/10/17/Hadoop-Had/","2023/10/17/Hadoop-Hexo安装/","2023/10/17/动漫-国漫/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/17/hello-world/","2023/10/17/Hadoop-Had/","2023/10/17/动漫-国漫/","2023/10/17/Hadoop-Hexo安装/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
