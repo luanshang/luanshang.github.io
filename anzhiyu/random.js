33ddae3b5df58b1cb6873dff10cdc98d92ca0294
luanshang <3092078599@qq.com>
2023-10-18 14:11:59 +0800

    Site updated: 2023-10-18 14:11:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/17/Hexo-Hexo安装/","2023/10/17/hello-world/","2023/10/17/动漫-国漫/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/17/hello-world/","2023/10/17/Hexo-Hexo安装/","2023/10/17/动漫-国漫/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
