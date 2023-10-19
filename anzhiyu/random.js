8a87a971ac7acd155b47548f524b304e39f5abb4
luanshang <3092078599@qq.com>
2023-10-19 14:05:50 +0800

    Site updated: 2023-10-19 14:05:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/18/交换路由-三层交换机/","2023/10/17/hello-world/","2023/10/17/动漫-国漫/","2023/10/17/Hexo-Hexo安装/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/17/Hexo-Hexo安装/","2023/10/17/动漫-国漫/","2023/10/17/hello-world/","2023/10/18/交换路由-三层交换机/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
