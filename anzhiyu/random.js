510003d4fc23e348c1f1563ef4ffd4d9d094c494
luanshang <3092078599@qq.com>
2023-10-25 21:22:08 +0800

    Site updated: 2023-10-25 21:22:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/17/hello-world/","2023/10/17/Hexo-Hexo安装/","2023/10/17/动漫-国漫/","2023/10/18/交换路由-三层交换机/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/17/hello-world/","2023/10/25/Clash-Clash汉化/","2023/10/17/Hexo-Hexo安装/","2023/10/18/交换路由-三层交换机/","2023/10/17/动漫-国漫/","2023/10/24/VSCode-VSCode添加快捷键Ctrl-Enter跳转下一行/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
